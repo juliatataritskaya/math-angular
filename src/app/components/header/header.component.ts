bbb578757ee0328e8f0b26d431f08fece5ef61a4
Julia Malko <julia.malko@hiendsys.com>
2019-04-18 11:40:02 +0300

    changed loading script;

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
-  constructor() {}
+  constructor(private elementRef: ElementRef) {
+  }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    let s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.innerHTML = 'console.log(\'done\');';
+    s.src = 'assets/js/script.js';
+    this.elementRef.nativeElement.appendChild(s);
   }
 
 }
